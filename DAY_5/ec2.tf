b42afa2051ddd7f51e1d285ec2fc830aa5a43fe8
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-15 22:40:35 +0530

    Update ec2.tf

--- DAY_5/ec2.tf
+++ DAY_5/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web_os" {
   
-ami = "ami-0d5eff06f840b45e9"
-instance_type = "t2.micro"
-key_name = "terrraform_key"
-security_groups = ["terraform-sg"]
+ ami = "ami-0d5eff06f840b45e9"
+ instance_type = "t2.micro"
+ key_name = "terrraform_key"
+ security_groups = ["terraform-sg"]
   
-tags = {
+ tags = {
   Name = "Web Server by TF"
  }
 }
