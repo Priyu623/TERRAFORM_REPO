017a4b56666d413572d9b9f9d379f48bd29c876f
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-15 22:41:48 +0530

    Update ebs_vol_attach.tf

--- DAY_5/ebs_vol_attach.tf
+++ DAY_5/ebs_vol_attach.tf
@@ -1,7 +1,7 @@
 resource "aws_volume_attachment" "ebs_attach" {
   
   device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.storage1.id
+  volume_id   = aws_ebs_volume.storage.id
   instance_id = aws_instance.web_os.id
   force_detach = true
 }
