db9bd8e7d6ba808264af13680e485a5a299b643e
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-21 18:11:17 +0530

    Create variables.tf

---
+++ DAY_7/multi-cloud/variables.tf
@@ -0,0 +1,69 @@
+# variable "x" {
+
+#    default = "t2.micro"
+#    type = string
+
+# }
+
+# variable "x" {}
+
+# variable "y" {
+
+#    type = bool
+
+# }
+
+# output "op1" {
+
+#    value = var.x
+
+# }
+
+# output "op2" {
+
+#    value = var.y ? "krish" : "tom"
+
+# }
+ 
+
+
+# LET'S CREATE OUR FINALL VARIABLES HERE
+
+variable "mtype" {}
+
+variable "istest" {
+  
+    type = bool
+}
+
+variable "azaws" {
+  
+    default = ["ap-south-1a", "ap-south-1b", "uap-south-1c"]
+    type    = list
+}
+
+variable "types" {
+  
+    type = map
+    default = {
+      
+        us-east-1  = "t2.micro",
+        ap-south-1 = "t2.nano",
+        us-west-1  = "t2.medium"
+    }
+  
+}
+
+
+output "op3" {
+  
+    value = var.azaws
+  
+}
+
+output "op4" {
+  
+    value = var.types["us-east-1"]
+  
+}
+
