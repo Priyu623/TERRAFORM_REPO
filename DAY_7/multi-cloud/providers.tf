a6ed60675a2889344235ba9fd49fe891d5aeb837
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-21 17:56:58 +0530

    Create providers.tf

---
+++ DAY_7/multi-cloud/providers.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  
+  region  = "us-east-1"
+  profile = "terraform"
+  
+}
+
+provider "google" {
+  
+  project     ="terraformprod2"
+  region      = "asia-south1"
+  zone        = "asia-south-c"
+  credentials = file("/Downloads/credentials.json")
+  
+}
