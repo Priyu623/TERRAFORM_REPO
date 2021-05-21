fd1eb701cc4ed66177ee9f70745f897bdfc2ef88
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-21 18:00:46 +0530

    Create gcp_main.tf

---
+++ DAY_7/multi-cloud/gcp_main.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "gcp_os" {
+  
+  name         = "os1"
+  machine_type = var.mtype
+  zone         = "asia-south-c"
+  count        = var.istest ? 1 : 0
+  
+  boot_disk {
+    
+    initialize_params {
+      image = "debian-cloud/debian-9"
+      
+    }
+  }
+  
+  network_interface {
+    
+    network = "default"
+    
+  }
+}
