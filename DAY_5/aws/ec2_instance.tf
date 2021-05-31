fbd72da4d15ec77ab2d91a205f0a484fe6120877
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-31 17:42:06 +0530

    Create ec2_instance.tf

---
+++ DAY_5/aws/ec2_instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web_os" {
+  
+ ami             = "ami-0d5eff06f840b45e9"
+ instance_type   = "t2.micro"
+ key_name        = "terrraform_key"
+ security_groups = ["terraform-sg"] 
+ tags = {
+  Name = "Web Server by TF"
+ }
+  
+}
