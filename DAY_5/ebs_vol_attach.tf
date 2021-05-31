13d94da6056deac9a03bcbef536b2f141bd16573
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-05-31 17:44:58 +0530

    Delete ebs_vol_attach.tf

--- DAY_5/ebs_vol_attach.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_volume_attachment" "ebs_attach" {
-  
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.storage.id
-  instance_id = aws_instance.web_os.id
-  force_detach = true
-}
