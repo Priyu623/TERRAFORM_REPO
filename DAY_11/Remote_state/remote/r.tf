de39e456238f9d90e4652a5617c8f84da9c37880
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-06-06 19:37:34 +0530

    Create r.tf

---
+++ DAY_11/Remote_state/remote/r.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  
+  region  = "us-east-1"
+  profile = "terraform"
+  
+}
+
+resource "aws_instance" "web" {
+  
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t3.micro"
+  
+  tags = {
+    Name = "HelloWorld"
+    
+  }
+  
+}
