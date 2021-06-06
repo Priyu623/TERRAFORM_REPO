cfab3792d1ed0558614d0da8b047375f791d78f6
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-06-06 18:50:34 +0530

    Create k8s.tf

---
+++ DAY_10/terra_function/kubernetes/k8s.tf
@@ -0,0 +1,24 @@
+provider "kubernetes" {
+
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+  
+}
+
+resource "kubernetes_pod" "test" {
+
+  metadata {
+    name = "mypod"
+  }
+
+  spec {
+  
+    container {
+      image = "vimal13/apache-webserver-php"
+      name  = "new_example"
+
+    }
+    
+  }
+  
+}
