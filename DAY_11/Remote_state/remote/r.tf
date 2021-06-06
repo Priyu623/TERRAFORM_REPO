957ea94b7aff18e689649b624f96d2c380a8dfba
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-06-06 19:41:59 +0530

    Update r.tf

--- DAY_11/Remote_state/remote/r.tf
+++ DAY_11/Remote_state/remote/r.tf
@@ -5,6 +5,18 @@ provider "aws" {
   
 }
 
+terraform {
+  
+  backend "s3" {
+    
+    bucket = "my"
+    key    = "my_tfstate"
+    region = "us-east-1"
+
+  }
+  
+}
+
 resource "aws_instance" "web" {
   
   ami           = "ami-0d5eff06f840b45e9"
