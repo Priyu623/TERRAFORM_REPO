543996357269188e0b36a67edd143214a70fa94d
Priya Soni <64746762+Priyu623@users.noreply.github.com>
2021-06-06 19:42:29 +0530

    Update r.tf

--- DAY_11/Remote_state/remote/r.tf
+++ DAY_11/Remote_state/remote/r.tf
@@ -17,14 +17,4 @@ terraform {
   
 }
 
-resource "aws_instance" "web" {
-  
-  ami           = "ami-0d5eff06f840b45e9"
-  instance_type = "t3.micro"
-  
-  tags = {
-    Name = "HelloWorld"
-    
-  }
-  
-}
+
